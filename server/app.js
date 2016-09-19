e5790a9207cd22d67f8277b8337ec3ee8e5174a3
Luis H. Matos <ReachLuis@Gmail.com>
2016-09-19 16:20:29 -0500

    /treats POST route successful

--- server/app.js
+++ server/app.js
@@ -48,17 +48,10 @@ app.get('/treats', function(req,res){
 }); // end app.get getRout
 
 
-// post route to receive information from client
-app.post('/routeName', function(req,res){
-  if (verbose) {console.log('Route /routeName hit', req.body);}
-  res.send('/routeName response. Received: '+req.body);
-});
-
 //post route to add information in Database
-app.post( '/postRouteA', function( req, res ){
-  if (verbose) {console.log( 'postRouteA route hit', req.body );}
-  //// --------NEED TO ENTER ***: SQL Query
-  var queryString = 'INSERT INTO <table> (<field1>, <field2>) VALUES (($1),($2));';
+app.post( '/treats', function( req, res ){
+  if (verbose) {console.log( 'treats post route hit', req.body );}
+  var queryString = 'INSERT INTO treat (name, description, pic) VALUES (($1),($2),($3));';
   if (verbose) {console.log('sending to database:', queryString);}
   //send queryString to database
   pg.connect(connectionString, function(err, client, done){
@@ -66,84 +59,9 @@ app.post( '/postRouteA', function( req, res ){
       if (verbose) {console.log(err);}
     }
     else{
-      client.query(queryString,[variableToReplaceBling]);
+      client.query(queryString,[req.body.name, req.body.description, req.body.url]);
       done();
       return res.sendStatus(200);
     }
   });
-});//end /postRouteA
-
-// post route that adds information in Database and also receives info
-app.post( '/postRouteB', function( req, res ){
-  if (verbose) {console.log( 'postRouteB route hit', req.body );}
-  //// --------NEED TO ENTER ***: SQL Query
-  var queryString = 'INSERT INTO <table> (<field1>, <field2>) VALUES (($1),($2));';
-  if (verbose) {console.log('sending to database:', queryString);}
-  //send queryString to database
-  pg.connect(connectionString, function(err, client, done){
-    if (err){
-      if (verbose) {console.log(err);}
-    }
-    else{
-      client.query(queryString,[variableToReplaceBling]);
-      //// --------NEED TO ENTER ***: SQL Query
-      var queryResult = client.query('***');
-      var resultsArray = [];
-      queryResult.on('row', function(row){
-        resultsArray.push(row);
-      });
-      queryResult.on('end', function(){
-        if (verbose) {console.log(resultsArray);}
-        done();
-        return res.json(resultsArray);
-      });
-    }
-  });
-});//end /postRouteB
-
-//post route to update information in Database
-app.put( '/putRouteA', function( req, res ){
-  if (verbose) {console.log( 'putRouteA route hit', req.body );}
-  //// --------NEED TO ENTER ***: SQL Query
-  var queryString = 'UPDATE <table> SET <field> = '+variable+' WHERE <field> = ($1);';
-  if (verbose) {console.log('sending to database:', queryString);}
-  //send queryString to database
-  pg.connect(connectionString, function(err, client, done){
-    if (err){
-      if (verbose) {console.log(err);}
-    }
-    else{
-      client.query(queryString,[variableToReplaceBling]);
-      done();
-      return res.sendStatus(200);
-    }
-  });
-});//end /putRouteA
-
-// put route that updates information in Database and also receives info
-app.put( '/putRouteB', function( req, res ){
-  if (verbose) {console.log( 'putRouteB route hit', req.body );}
-  //// --------NEED TO ENTER ***: SQL Query
-  var queryString = 'UPDATE <table> SET <field> = '+variable+' WHERE <field> = ($1);';
-  if (verbose) {console.log('sending to database:', queryString);}
-  //send queryString to database
-  pg.connect(connectionString, function(err, client, done){
-    if (err){
-      if (verbose) {console.log(err);}
-    }
-    else{
-      client.query(queryString,[variableToReplaceBling]);
-      //// --------NEED TO ENTER ***: SQL Query
-      var queryResult = client.query('***');
-      var resultsArray = [];
-      queryResult.on('row', function(row){
-        resultsArray.push(row);
-      });
-      queryResult.on('end', function(){
-        if (verbose) {console.log(resultsArray);}
-        done();
-        return res.json(resultsArray);
-      });
-    }
-  });
-});//end /putRouteB
+});//end /treats post
